a800a72a4005912c10accfd8fdcbb13be4378aba
Kunal Gupta <kunal@changejar.in>
2023-10-19 01:59:13 +0530

    poc

--- app/src/main/java/com/aregyan/compose/ui/details/DetailsScreen.kt
+++ app/src/main/java/com/aregyan/compose/ui/details/DetailsScreen.kt
@@ -1,14 +1,26 @@
 package com.aregyan.compose.ui.details
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.aregyan.compose.R
 import com.aregyan.compose.ui.components.NoNetwork
 
 @Composable
@@ -19,34 +31,56 @@ fun DetailsScreen() {
     if (uiState.offline) {
         NoNetwork()
     } else {
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-                .padding(16.dp)
-        ) {
-            AsyncImage(
-                modifier = Modifier.size(100.dp),
-                model = uiState.detail.avatar,
-                contentDescription = null
-            )
-            Column {
-                Text(
-                    modifier = Modifier.padding(start = 16.dp),
-                    text = uiState.detail.name.orEmpty(),
-                    color = MaterialTheme.colorScheme.onBackground
+        Box(Modifier.fillMaxSize()) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(dimensionResource(id = com.intuit.sdp.R.dimen._16sdp))
+                    .padding(dimensionResource(id = com.intuit.sdp.R.dimen._16sdp))
+            ) {
+                AsyncImage(
+                    modifier = Modifier.size(dimensionResource(id = com.intuit.sdp.R.dimen._100sdp)),
+                    model = uiState.detail.avatar,
+                    contentDescription = null
                 )
-                Text(
-                    modifier = Modifier.padding(start = 16.dp, top = 16.dp),
-                    text = uiState.formattedUserSince,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-                Text(
-                    modifier = Modifier.padding(start = 16.dp, top = 16.dp),
-                    text = uiState.detail.location.orEmpty(),
-                    color = MaterialTheme.colorScheme.onBackground
-                )
+                Column {
+                    Text(
+                        modifier = Modifier.padding(start = dimensionResource(id = com.intuit.sdp.R.dimen._16sdp)),
+                        text = uiState.detail.name.orEmpty(),
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontSize = dimensionResource(id = com.intuit.ssp.R.dimen._12ssp).value.sp
+                    )
+                    Text(
+                        modifier = Modifier.padding(
+                            start = dimensionResource(id = com.intuit.sdp.R.dimen._16sdp),
+                            top = dimensionResource(id = com.intuit.sdp.R.dimen._16sdp)
+                        ),
+                        text = uiState.formattedUserSince,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontSize = dimensionResource(id = com.intuit.ssp.R.dimen._12ssp).value.sp
+                    )
+                    Text(
+                        modifier = Modifier.padding(start = dimensionResource(id = com.intuit.sdp.R.dimen._16sdp), top = dimensionResource(id = com.intuit.sdp.R.dimen._16sdp)),
+                        text = uiState.detail.location.orEmpty(),
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontSize = dimensionResource(id = com.intuit.ssp.R.dimen._12ssp).value.sp
+                    )
+                }
+            }
+
+            Row(Modifier.fillMaxSize()) {
+                Spacer(Modifier.weight(1f))
+                Column(
+                    Modifier
+                        .width(dimensionResource(id = com.intuit.sdp.R.dimen._10sdp))
+                        .fillMaxHeight()
+                        .background(Color.Red)) {
+
+                }
+                Spacer(Modifier.weight(1f))
             }
         }
+
     }
 }
